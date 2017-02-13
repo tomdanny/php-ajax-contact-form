bbd33b4b68693ecdb19165f52219a14ce3909bdd
Daniel Tomic <danieltomic@Daniels-MacBook-Pro.local>
2017-02-13 07:49:40 -0500

    php form submition done

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Form</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="form.css" type="text/css">
+</head>
+<body>
+
+<!-- <button id="click">click</button>
+
+<div id="load">
+
+</div> -->
+
+<?php include_once "form_process.php";?>
+
+<div class="container">  
+  <form id="contact" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+    <h3>Contact</h3>
+    <h4>Contact us today, and get reply with in 24 hours!</h4>
+    <fieldset>
+      <input placeholder="Your name" type="text" name="name" value="<?= $name ?>" tabindex="1" autofocus>
+      <span class="error"><?= $name_error ?></span>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Email Address" type="text" name="email" value="<?= $email ?>" tabindex="2">
+      <span class="error"><?= $email_error ?></span>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Phone Number" type="text" name="phone" value="<?= $phone ?>" tabindex="3">
+      <span class="error"><?= $phone_error ?></span>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Web Site starts with http://" type="text" name="url" value="<?= $url ?>" tabindex="4" >
+      <span class="error"><?= $url_error ?></span>
+    </fieldset>
+    <fieldset>
+      <textarea value="<?= $message ?>" name="message" tabindex="5">
+      </textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+    <div class="success"><?= $success ?></div>
+  </form>
+</div>
+	
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="script.js"></script>
+</body>
+</html>
